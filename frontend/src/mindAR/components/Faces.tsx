c5c6e2e97a5eb3a38af0992508e16235b66b746e
최정온 <chjo0330@gmail.com>
2023-02-02 02:07:33 +0900

    [UPDATE] Faces.tsx import 디렉토리 수정

--- frontend/src/mindAR/components/Faces.tsx
+++ frontend/src/mindAR/components/Faces.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Entity } from 'aframe-react-component';
 import { Entity as AEntity } from 'aframe';
-import { Faces as _Faces } from '../mindAR/utils/interfaces';
+import { Faces as _Faces } from '../utils/interfaces';
 
 const Faces = React.forwardRef<AEntity, _Faces>(({ anchorIndex, ...props }, ref) => (
   <Entity {...props} mindar-face-target={`anchorIndex: ${anchorIndex}`} ref={ref} />
