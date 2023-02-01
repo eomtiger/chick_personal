3f1861bd030605180804bdb72d544dca2b970a00
최정온 <chjo0330@gmail.com>
2023-02-02 02:18:53 +0900

    [UPDATE] 홈 화면 CSS 수정

--- frontend/src/components/organism/FacePlay/index..jsx
+++ frontend/src/components/organism/FacePlay/index..jsx
@@ -1,12 +1,12 @@
-import FacePlayHomeBox from "../../molecules/FacePlayHomeBox";
-import CommonBtn from "./../../atoms/CommonBtn/index";
+import FacePlayHomeBox from '../../molecules/FacePlayHomeBox';
+import CommonBtn from './../../atoms/CommonBtn/index';
 
 function FacePlay({ children }) {
   return (
-    <div className="flow flow-col w-[1076px] h-[100%]">
+    <div className="absolute left-48 w-[1076px] h-[100%]">
       <div className="flex justify-end">
-        <CommonBtn text={"회원가입"} />
-        <CommonBtn text={"로그인"} color="yellow" />
+        <CommonBtn text={'회원가입'} />
+        <CommonBtn text={'로그인'} color="yellow" />
       </div>
       <FacePlayHomeBox />
     </div>
