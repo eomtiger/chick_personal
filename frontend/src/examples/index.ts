6e736e8474a92d1841f46ddff46d7a0c10e0edf6
최정온 <chjo0330@gmail.com>
2023-02-02 01:51:49 +0900

    [UPDATE] 안쓰는 import문 삭제

--- frontend/src/examples/index.ts
+++ frontend/src/examples/index.ts
@@ -1,5 +1,3 @@
 import FaceTracking from './FaceTracking';
-import ImageTracking from './ImageTracking';
-import ImageCompiler from './ImageCompiler';
 
-export { FaceTracking, ImageTracking, ImageCompiler };
+export { FaceTracking };
