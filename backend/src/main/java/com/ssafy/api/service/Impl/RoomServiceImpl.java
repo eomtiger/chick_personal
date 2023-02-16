d872e544dd853debbf5c3b1c294784fca456d70c
juseong <kjschocolate@gmail.com>
2023-02-16 20:30:16 +0900

    [FIX] 매칭 알고리즘 수정

--- backend/src/main/java/com/ssafy/api/service/Impl/RoomServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/Impl/RoomServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Random;
 
 @Service
 public class RoomServiceImpl implements RoomService {
@@ -59,8 +60,10 @@ public class RoomServiceImpl implements RoomService {
 
         // 참가할 수 있는 게임방이 없으면 새로운 세션 생성
         // newSession := [회원/게스트][게임종류]Session[방번호]
-        String newSession = guest + gameType + "Session" + roomRepository.count();
+//        String newSession = guest + gameType + "Session" + roomRepository.count();
 //        String newSession = guest + gameType + "Session" + (++roomNo);
+        Random random = new Random();
+        String newSession = guest + gameType + "Session" + random.nextInt();
 
         // 새로운 게임방 생성
         roomRepository.save(Room.builder()
